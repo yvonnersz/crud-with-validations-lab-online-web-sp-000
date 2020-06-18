eede673f544df8d1dd969f955a322de28bd9aeb3
Yvonne M Pham <yvonnersz@gmail.com>
2020-06-18 09:20:20 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,6 +1,10 @@
 class SongsController < ApplicationController
   before_action :set_song, only: [:show, :edit, :update, :destroy]
 
+  def index
+    @songs = Song.all
+  end
+
   def new
     @song = Song.new
   end
