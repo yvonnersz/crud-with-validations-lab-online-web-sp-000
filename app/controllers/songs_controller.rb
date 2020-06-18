1e7a57c6c6ebb1b3c4f1970283683ec7260e808c
Yvonne M Pham <yvonnersz@gmail.com>
2020-06-18 09:16:11 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,5 +1,5 @@
 class SongsController < ApplicationController
-  before_action :set_song, only: [:show, :edit, :update]
+  before_action :set_song, only: [:show, :edit, :update, :destroy]
 
   def new
     @song = Song.new
